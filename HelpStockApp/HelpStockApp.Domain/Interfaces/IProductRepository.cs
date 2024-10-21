b9a579f17ee8543193196888b3fd4d8e6953c5b6
murarirc <raphaelmcaceres@gmail.com>
2024-10-21 10:23:08 -0300

    Aula1021 - Interfaces de Domain

---
+++ HelpStockApp/HelpStockApp.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using HelpStockApp.Domain.Entities;
+
+namespace HelpStockApp.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetById(int? id);
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(Product product);
+    }
+}
